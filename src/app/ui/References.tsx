b1549b32453c59bf9ee1d012bb0102d3208374dd
Benjamin Yackley <113474767+beyackle2@users.noreply.github.com>
2023-08-28 15:50:18 -0700

    start making references into buttons

--- src/app/ui/References.tsx
+++ src/app/ui/References.tsx
@@ -15,8 +15,13 @@ export default memo(({ references }: Props) => {
   return (
     <ul>
       {references.map(reference => (
-        <li>
-          <pre>{JSON.stringify(reference, null, 2)}</pre>
+        <li className='outerBox'>
+          <span className='innerBox'>{reference.id}</span>
+          <span className='link'>
+            {
+              reference.url ? <a target="_blank" rel="noopener noreferrer" href={reference.url}>{reference.title}</a> : <button>{reference.title}</button>
+            }
+          </span>
         </li>
       ))}
     </ul>
