21f3c0406e7e4a8d311a119cc9d4eda9b3c6fdea
Benjamin Yackley <113474767+beyackle2@users.noreply.github.com>
2023-08-30 16:12:36 -0700

    revise sample activity

--- src/app/data/sampleActivity.ts
+++ src/app/data/sampleActivity.ts
@@ -17,44 +17,22 @@ export default {
 
       text: `## Introduction
 
-      Configuring proxy settings is a fundamental aspect of network and system administration. Proxies serve as intermediaries between a user's device and the internet, providing various benefits such as improved security, anonymity, and network performance. In this guide, we will delve into the intricacies of configuring Proxy Auto-Discovery (PAD) proxy settings. We'll explore what PAD is, why it's essential, and how to configure it effectively.
+      *Configuring* **proxy** _settings_ __is__ a fundamental aspect of \`network\` and [system administration](http://example.com). Proxies serve as intermediaries between a user's device and the internet, providing various benefits such as improved security, anonymity, and network performance. In this guide, we will delve into the intricacies of configuring Proxy Auto-Discovery (PAD) proxy settings. We'll explore what PAD is, why it's essential, and how to configure it effectively. []
+      > This is a block quote!
+
+      1. first
+      1. second
+      1. third
+
+      - unordered A
+      - unordered B
+      - unordered C
+
+      ***
 
       ## Understanding Proxy Auto-Discovery (PAD)
 
       Proxy Auto-Discovery, often abbreviated as PAD, is a mechanism that simplifies the process of configuring proxy settings for network-connected devices. Its primary purpose is to automate the discovery of proxy servers and settings without manual intervention. PAD relies on a specific protocol called Web Proxy Auto-Discovery (WPAD).
-
-      ## The Importance of Configuring PAD Proxy Settings
-
-      Configuring PAD proxy settings is crucial for several reasons:
-
-      - Efficiency: PAD eliminates the need for users or administrators to manually configure proxy settings on individual devices. This automation saves time and reduces the risk of configuration errors.
-      - Scalability: In large organizations with numerous devices, configuring proxies manually can be a daunting task. PAD simplifies this process, making it manageable even in complex network environments.
-      - Consistency: Automated configuration ensures that all devices in the network use consistent proxy settings, reducing potential discrepancies and ensuring uniform security measures.
-      - Security: Properly configured proxy settings can enhance network security by filtering and monitoring traffic, blocking malicious websites, and protecting against threats like malware and phishing.
-      - Anonymity: For users who require anonymity when browsing, PAD can route traffic through anonymous proxies, safeguarding their identity online.
-
-      ## Configuring PAD Proxy Settings
-
-      Now, let's delve into the steps to configure PAD proxy settings effectively:
-
-      - Identify Your Proxy Server: Before configuring PAD, you need to identify the proxy server(s) you want to use. This could be an in-house proxy server or a third-party service.
-      - Ensure WPAD Support: Verify that your network environment supports the Web Proxy Auto-Discovery (WPAD) protocol. This typically involves setting up a WPAD server or ensuring that your existing DHCP and DNS servers can provide WPAD information.
-      - Create a PAC File: A PAC (Proxy Auto-Configuration) file contains instructions for the device to determine when and how to use the proxy server. You can create a PAC file using a text editor, and it should be hosted on a web server accessible to all devices in your network.
-      - Configure DHCP: If your network uses DHCP (Dynamic Host Configuration Protocol), you can configure it to provide the URL of the PAC file to client devices. This simplifies the configuration process as devices will automatically fetch the PAC file.
-      - Configure DNS: Similarly, DNS (Domain Name System) can be configured to resolve the WPAD host (e.g., wpad.yourdomain.com) to the location of the PAC file. This allows devices to locate the PAC file without manual intervention.
-      - Test Configuration: After configuring DHCP and DNS, it's essential to test the configuration to ensure that devices can access the PAC file correctly. You can do this by attempting to access the PAC file URL from a client device's web browser.
-      - Configure Devices: For devices that do not support DHCP or WPAD, you may need to configure proxy settings manually. This typically involves entering the PAC file URL or specifying the proxy server address and port in the device's settings.
-      - Monitor and Maintain: Regularly monitor your proxy configuration to ensure that it continues to function correctly. Update the PAC file as needed to reflect changes in your network or proxy server settings.
-
-      ## Common Challenges and Troubleshooting
-
-      While configuring PAD proxy settings, you may encounter some common challenges:
-
-      - Firewall Issues: Ensure that your network's firewall rules permit traffic to and from the PAC file server and proxy server(s).
-      - DNS Resolution Problems: Verify that DNS is correctly configured to resolve the WPAD host to the PAC file's location.
-      - PAC File Errors: Carefully review the PAC file for syntax errors or incorrect instructions that could disrupt the proxy configuration.
-      - Device Compatibility: Some devices may not fully support WPAD, requiring manual configuration. Be prepared to address these exceptions.
-      - Security Concerns: Maintain robust security measures to prevent unauthorized access to your PAC file or proxy servers.
 
       ## Conclusion
 
