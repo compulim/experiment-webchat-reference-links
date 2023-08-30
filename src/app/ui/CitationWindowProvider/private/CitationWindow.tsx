5e9fb247ac94cb25857e7c271c06e867e483121b
Benjamin Yackley <113474767+beyackle2@users.noreply.github.com>
2023-08-30 15:47:33 -0700

    format HTML within citation window [WIP]

--- src/app/ui/CitationWindowProvider/private/CitationWindow.tsx
+++ src/app/ui/CitationWindowProvider/private/CitationWindow.tsx
@@ -1,3 +1,7 @@
+import { hooks } from 'botframework-webchat';
+
+import renderMarkdownAsHTML from '../../private/renderMarkdownAsHTML';
+
 import './CitationWindow.css';
 
 type Props = {
@@ -5,13 +9,25 @@ type Props = {
   onClose: () => void;
 };
 
+const { useLocalizer, useStyleOptions } = hooks;
+
 const CitationWindow = ({ text, onClose }: Props) => {
+  const [styleOptions] = useStyleOptions();
+  const localize = useLocalizer();
+
+  const externalLinkAlt = localize('MARKDOWN_EXTERNAL_LINK_ALT');
+
   return (
     <div className="mainWindow">
       <button className="closeBox" onClick={onClose} tabIndex={0}>
         &times;
       </button>
-      <span className="contents">{text}</span>
+      <span
+        className="contents"
+        dangerouslySetInnerHTML={{
+          __html: renderMarkdownAsHTML(text || '', styleOptions, { externalLinkAlt })
+        }}
+      />
     </div>
   );
 };
