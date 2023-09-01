e2f9f2af4362ecc9a5bfb3c24f323e4124de90c5
William Wong <compulim@hotmail.com>
2023-08-31 18:33:51 -0700

    Rename CSS class

--- src/app/ui/ActivityStatus/private/SlottedActivityStatus.tsx
+++ src/app/ui/ActivityStatus/private/SlottedActivityStatus.tsx
@@ -8,7 +8,7 @@ type Props = PropsWithChildren<{
 }>;
 
 const SlottedActivityStatus = memo(({ children, className }: Props) => (
-  <span className={classNames('pva-web-chat__slotted-activity-status', className)}>
+  <span className={classNames('webchat__slotted-activity-status', className)}>
     {Children.map(children, (child, index) =>
       index ? (
         <Fragment>
